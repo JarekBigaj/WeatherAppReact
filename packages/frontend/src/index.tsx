1aaba28876d12df852da0a5b5fdbb286dc849ffc
Jarek Bigaj <j.bigaj@interia.pl>
2023-07-12 19:19:32 +0200

    init node server

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
